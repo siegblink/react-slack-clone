8ce4307771826dbf12bcb99755e0fac4112d60f8
Siegfred Balona <siegblink@gmail.com>
2019-07-18 08:31:30 +0800

    Update react to the latest version and add firebase config in gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,8 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 import 'firebase/storage'
+import { firebaseConfig } from './config'
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyC7wKZOfi0BzMllX7zaNjxzKdOQwjBbYqM',
-  authDomain: 'react-slack-clone-d4f46.firebaseapp.com',
-  databaseURL: 'https://react-slack-clone-d4f46.firebaseio.com',
-  projectId: 'react-slack-clone-d4f46',
-  storageBucket: 'react-slack-clone-d4f46.appspot.com',
-  messagingSenderId: '864520592841',
-  appId: '1:864520592841:web:357471a369eb5135',
-}
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
